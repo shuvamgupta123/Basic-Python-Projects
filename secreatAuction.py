c5525a4971fff061d6b47d581c48047f1af8c41e
Shuvam Gupta <143183616+shuvamgupta123@users.noreply.github.com>
2026-07-04 18:43:49 +0545

    Implement bidding functionality in secreatAuction.py

--- secreatAuction.py
+++ secreatAuction.py
@@ -1,1 +1,17 @@
+bidder={}
+name = ""
+benchmarkamt = 0
+quit = "y"
+while quit == "y":
+    a = input("What is bidder's name?")
+    b = input("What is the bid amount?")
+    bidder[a] = b
+    quit = input("Any other bidder(Y/n)?").lower()
+    print("\n"*50)
 
+for i in bidder:
+    if int(bidder[i]) > benchmarkamt:
+        name = i
+        benchmarkamt = int(bidder[i])
+
+print(f"{name} won the bid with the highest bid of {benchmarkamt}")
