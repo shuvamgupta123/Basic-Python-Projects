5c88a1c3d07e351b3b55fa0a237a7febafb99638
shuvamgupta123 <sg750053@gmail.com>
2026-09-02 23:57:33 +0545

    final coffee machine

--- Coffee-Machine/main.py
+++ Coffee-Machine/main.py
@@ -1,9 +1,25 @@
 from data import menu, resources
 profit = 0
 dataToIgnore = ["water", "milk", "coffee", "report", "off"]
+amount = 0
+
+def process_coins():
+    print("Please insert coins.")
+    total = int(input("How many quarters?: ")) * 0.25
+    total += int(input("How many dimes?: ")) * 0.10 
+    total += int(input("How many nickels?: ")) * 0.05
+    total += int(input("How many pennies?: ")) * 0.01
+    return total
+
+def is_transaction_successful(payment, cost):
+    if payment >= cost:
+        return True
+    else:
+        return False
 
 while True:
     coffee = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    amount = menu[coffee]["cost"]
 
     if coffee == "off":
         print("Turning off the coffee machine. Goodbye!")
@@ -21,8 +37,17 @@ while True:
     if coffee in menu:
         drink = menu[coffee]                    #{all drinks}
         ingredients = drink["ingredients"]      #{all ingredients for the selected drink}
-
         for ingredient in ingredients:
             if ingredients[ingredient] > resources[ingredient]:
                 print(f"Sorry, there is not enough {ingredient}.")
                 break
+
+    else:
+        print(f"The cost of {coffee} is ${amount}.")
+        payment = process_coins()
+        if is_transaction_successful(payment, amount):
+            change = round(payment - amount, 2)
+            print(f"Here is ${change} in change.")
+            profit += amount
+        else:
+            print("Sorry, that's not enough money. Money refunded.")
